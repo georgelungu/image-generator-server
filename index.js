e3913108acfd02c02b3157babab6597965f74f7c
George Lungu <george.lungu1896@gmail.com>
2023-11-05 22:17:38 +0200

    created a mongoose schema and model for posts and 2 files with boilerplate code for 2 routes, the post route and the dalle route but it will throw error because i have imports with no default export in the routes files

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
-import connectDB from './mongodb/connect'
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
-// left at 03:23:50
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
 
 // Ensure the app is running once we visit the URL of the root page.
 app.get('/', async (req, res) =>
--- mongodb/connect.js
+++ mongodb/connect.js
@@ -4,10 +4,11 @@ const connectDB = (url) =>
 {
     // useful when we will work with search functionlity latah.
     mongoose.set('strictQuery', true)
+    
+    mongoose.connect(url)
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err))
 }
 
-mongoose.connect(url)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err))
 
 export default connectDB
