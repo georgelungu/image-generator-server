15497ec9792199ba1dae6e3642fd36e488c9e5b5
George Lungu <george.lungu1896@gmail.com>
2023-11-05 23:09:50 +0200

    created default exports for dalleRoutes and postRoutes, also created post route in dalleRoutes in order to generate image from the user prompt, not yet added the connection to frontend

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -1,7 +1,52 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from 'openai' 
+import OpenAI from 'openai';
 
 dotenv.config()
 
 const router = express.Router()
+
+// use the api key
+const configuration = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+// create an instance of openai
+const openai = new OpenAI(configuration)
+
+// demo route
+router.route('/').get((req, res) =>
+{
+    res.send("Hello from dalleRoutes.js file!")
+})
+
+router.route('/').post(async (req, res) => 
+{
+    try 
+    {
+        // the prompt sent by the user from the frontend
+        const { prompt } = req.body
+
+        // generate an image with the following parameters
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        })
+
+        // get the image
+        const image = aiResponse.data.data[0].b64_json;
+
+        // send the image
+        res.status(200).json({ photo: image })
+    } 
+    catch (error) 
+    {
+        console.log(error)
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
+
+export default router
